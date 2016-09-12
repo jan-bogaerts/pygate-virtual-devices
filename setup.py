c2decb94177c833471061629cdf05fecc6058c4b
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 17:18:22 +0200

    initial checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_virtual_devices',
+    version='1.0',
+    packages=['pygate_virtual_devices'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description='plugin for the pygate: add virtual devices to the gateway such as online weather stations'
+)
